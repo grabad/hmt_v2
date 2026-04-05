510255f397161c708c55766bebaf5f56a9dbf39a
grabad <128432541+grabad@users.noreply.github.com>
2026-04-05 15:12:46 -0400

    added z quantile filtering

--- hmt_functions.py
+++ hmt_functions.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 import napari
 from sklearn.cluster import DBSCAN
 
+def filter_quantile(df, percent=0.05, column="z [nm]"):
+    lower_bound = df[column].quantile(percent)
+    upper_bound = df[column].quantile(1 - percent)
+    
+    filtered_df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+    
+    return filtered_df
+
+
 def cluster_dbscan(loc_df, eps=50, min_samples=3):
     loc_np = loc_df[["x [nm]", "y [nm]", "z [nm]"]].to_numpy()
     loc_clust = DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1).fit_predict(loc_np)    
@@ -25,3 +34,54 @@ def cluster_dbscan(loc_df, eps=50, min_samples=3):
             loc_colors.append(loc_cluster_colors[c])
             
     loc_df["cluster_color"] = loc_colors
+
+def plot_clusters_napari(me3_df, ac_df, scaling_factor=10):
+    me3_df["plot_size"] = me3_df['sigmax [nm]'].to_numpy()/110*scaling_factor
+    ac_df["plot_size"] = ac_df['sigmax [nm]'].to_numpy()/110*scaling_factor
+
+    coords = ["x [nm]", "y [nm]", "z [nm]"]
+    me3_border = [0, 0.7, 0, 1]
+    ac_border = [0.7, 0, 0, 1]
+
+    me3_nano = me3_df[me3_df["cluster_label"] != -1]
+    me3_noise = me3_df[me3_df["cluster_label"] == -1]
+    ac_nano = ac_df[ac_df["cluster_label"] != -1]
+    ac_noise = ac_df[ac_df["cluster_label"] == -1]
+
+    viewer = napari.Viewer()
+    viewer.add_points(
+        data=ac_noise[coords],
+        features=ac_noise,
+        face_color=ac_noise["cluster_color"].tolist(),
+        border_color=ac_border, 
+        border_width=0.01, 
+        size=ac_noise["plot_size"],    
+        name="H3K27ac Noise"
+    )           
+    viewer.add_points(
+        data=me3_noise[coords],
+        features=me3_noise,
+        face_color=me3_noise["cluster_color"].tolist(), 
+        border_color=me3_border, 
+        border_width=0.01, 
+        size=me3_noise["plot_size"],   
+        name="H3K27me3 Noise"
+    )                     
+    viewer.add_points(
+        data=ac_nano[coords],
+        features=ac_nano,
+        face_color=ac_nano["cluster_color"].tolist(), 
+        border_color=ac_border, 
+        border_width=0.01, 
+        size=ac_nano["plot_size"],    
+        name="H3K27ac Nanodomains"
+    )           
+    viewer.add_points(
+        data=me3_nano[coords],
+        features=me3_nano,
+        face_color=me3_nano["cluster_color"].tolist(),
+        border_color=me3_border, 
+        border_width=0.01, 
+        size=me3_nano["plot_size"],   
+        name="H3K27me3 Nanodomains"
+)
